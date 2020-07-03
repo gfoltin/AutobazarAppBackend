a4c862e348fffe147ec385700dc110aa43eaf921
Gustav Foltin <gusto1802199@gmail.com>
2020-07-03 16:50:15 +0200

    update

--- src/main/java/GustoGroup/AutobazarBackend/CarJPARepository.java
+++ src/main/java/GustoGroup/AutobazarBackend/CarJPARepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//Repository for Car class
 @Repository
 public interface CarJPARepository extends JpaRepository<Car, Long> {
 	List<Car> findByUsername(String username);
