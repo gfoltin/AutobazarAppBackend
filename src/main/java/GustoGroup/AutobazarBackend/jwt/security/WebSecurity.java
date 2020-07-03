c01c9b5972d117c9791b4aa6a6d5bd791cec5dff
Gustav Foltin <gusto1802199@gmail.com>
2020-07-03 17:03:06 +0200

    update security

--- src/main/java/GustoGroup/AutobazarBackend/jwt/security/WebSecurity.java
+++ src/main/java/GustoGroup/AutobazarBackend/jwt/security/WebSecurity.java
@@ -36,7 +36,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.cors().and()
         		.authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .antMatchers("/console/**").permitAll()
